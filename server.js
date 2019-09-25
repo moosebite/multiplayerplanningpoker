cf90897d8f448c6f8d83d1e112bf112e11f99490
Steven Hearne <smh9w@mtmail.mtsu.edu>
2019-09-25 10:44:44 -0500

    reworked the previous files and got them working

--- server.js
+++ server.js
@@ -3,22 +3,21 @@
 // Variable Instantiations
 const express = require('express');
 const app = express();
-const http = require("http");
-const io = require('socket.io')(http)
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+app.get('/', function(req, res) {
+	res.render('server.ejs');
+});
 // Create server
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.end('This should prompt for your username.');
-}).listen(8080,10,82,67,214)
-
-// Socket implementation
-io.sockets.on('connect', function(socket) {
+io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit(socket.username);
-    })
-})
+        io.emit('is_online', socket.username);
+    });
 
+});
 
-console.log('Listening on :8080');
+const server = http.listen(8080, '10.82.64.65' || 'localhost', function() {
+    console.log('listening on *:8080');
+});
