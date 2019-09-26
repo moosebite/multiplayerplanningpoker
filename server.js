0321bcbab1fdef768d4fc9b9dd45e466bf969436
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-09-26 14:19:42 -0500

    Updated in class

--- server.js
+++ server.js
@@ -5,19 +5,26 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var userCount = 0;
 
 app.get('/', function(req, res) {
 	res.render('server.ejs');
 });
+
+var usernameList = [];
+
 // Create server
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online', socket.username);
+        usernameList.push(username);
+        console.log(usernameList[userCount]);
+        io.emit('currentUsernames', usernameList, usernameList.length);
+        userCount++;
     });
 
 });
 
-const server = http.listen(8080, '10.82.64.65' || 'localhost', function() {
+const server = http.listen(8080, '10.82.69.1' || 'localhost', function() {
     console.log('listening on *:8080');
 });
