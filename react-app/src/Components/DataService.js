39a3d3c8dbc0cfbbc80a4ce1dd705bfe54399093
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-10-08 18:18:40 +0000

    Adding a DataService file

---
+++ react-app/src/Components/DataService.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import socket from 'socket.io'
+
+fucntion UpdateVote() {
+    return(
+    
+    );
+}
+
+fucntion ClearVote() {
+    return(
+    
+    );
+}
+
+fucntion Username() {
+    return(
+    
+    );
+}
+
+export default DataService
