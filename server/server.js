cfb613f68eebfdff0367d2d30959e2fa87e9149d
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-10-10 11:26:05 -0500

    Updated spelling errors in comments. Updated functions to camelCase.

--- server/server.js
+++ server/server.js
@@ -10,39 +10,39 @@ var users = [];
 
 // on Connection
 io.sockets.on('connection', function(socket) {
-        // when the user opject is given adding it to the users array and updating the clints
+        // When the user object is given adding it to the users array and updating the clients
     socket.on('username', function(username) {
-        users.push({username : username, GM = false, vote : 0, socketid : socket.id});
+        users.push({username : username, GM = false, vote : 0, socketId : socket.Id});
         users.length == 1 ? users[0].GM = true : null
         UpDate(users);
     });
-        // remove user from array when they disconnect
-        // removing the user that has disconneced
-        //updating the clints
+        // Remove user from array when they disconnect
+        // Removing the user that has disconnected
+        // Updating the clients
     socket.on('disconnect', function () {
-        usersnew = users.filter((user) => user.socketid != socket.id);
-        UpDate(usersnew);
+        usersNew = users.filter((user) => user.socketId != socket.Id);
+        UpDate(usersNew);
      });
 
 });
-    //calling the update function when a clint requsts it
-    //The function that updates the clints when there has been a change to the users array
-    //moving the updated array to the users array
-    //emiting the updated users array to the clints
-    //checking if all votes are in if so showing votes on the clints
+    // Calling the update function when a clint requests it
+    // The function that updates the clients when there has been a change to the users array
+    // Moving the updated array to the users array
+    // Emitting the updated users array to the clients
+    // Checking if all votes are in if so showing votes on the clients
 io.sockets.on('update', UpDate);
 
-function UpDate(usersupdate) {
-    users = usersupdate;
+function UpDate(usersUpdate) {
+    users = usersUpdate;
     io.emit('current', users);
     !users.some((current) => current.vote === null) ? io.emit('showVotes') : null;
 };
-    //when the game master requsts the votes to be cleared 
-    //removing the votes off the clints screens
+    //when the game master requests the votes to be cleared 
+    //removing the votes off the clients screens
     //resetting all votes to null
-    //calling the update function to give the updated array to the clints
-io.sockets.on('clearvotes', function(){ 
-    io.emit('hidevotes');
+    //calling the update function to give the updated array to the clients
+io.sockets.on('clearVotes', function(){ 
+    io.emit('hideVotes');
     users.forEach((current) => current.vote = null);
     UpDate(users);
 });
