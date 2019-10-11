415f4b07e581b78f500e7adb3062c25d6336cac1
Steven Hearne <smh9w@mtmail.mtsu.edu>
2019-10-11 15:56:16 -0500

    Changed userList to a Map from a list

---
+++ react-app/src/Utils/DataService.js
@@ -0,0 +1,28 @@
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:8080')
+
+function updateVote(vote) {
+    io.emit('updateVote', vote);
+}
+
+function clearVotes() {
+    io.emit('clearVotes');
+}
+
+function addUser(username) {
+    io.emit('addUser', username);
+}
+
+function updateUserList(callback) {
+    socket.on('current', newUserList => {
+        callback(newUserList);
+        console.log('Received new userList.');
+    });
+}
+
+function toggleVotes(callback) {
+    socket.on('toggleVotes', () => callback());
+}
+
+export { updateVote, clearVotes, addUser, updateUserList, toggleVotes }
--- server/server.js
+++ server/server.js
@@ -1,52 +1,52 @@
-// Server for the multiplayer planning poker
-
 // Variable Instantiations
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')();
 
-var users = [];
+var PORT = 8080;
+
+var userList = new Map();
+
+var gmChosen = false;
 
 // on Connection
 io.sockets.on('connection', function(socket) {
-        // When the user object is given adding it to the users array and updating the clients
-    socket.on('username', function(username) {
-        users.push({username : username, GM = false, vote : 0, socketId : socket.Id});
-        users.length == 1 ? users[0].GM = true : null
-        UpDate(users);
+
+    socket.on('addUser', function(username) {
+        userList[socket.id] = {username : username, GM = false, vote : null};
+        if(!gmChosen)
+        {
+            userList[socket.id].GM = true;
+            gmChosen = true;
+        }
+        upDate();
     });
-        // Remove user from array when they disconnect
-        // Removing the user that has disconnected
-        // Updating the clients
+
+    socket.on('updateVote', (vote) => {
+        userList[socket.id].vote = vote;
+        upDate();
+    })
+    
+    socket.on('clearVotes', function(){ 
+        io.emit('toggleVotes');
+        Object.keys(userList).map((key) => userList[key].vote = null);
+        upDate();
+    });
+    
     socket.on('disconnect', function () {
-        usersNew = users.filter((user) => user.socketId != socket.Id);
-        UpDate(usersNew);
-     });
+        userList[socket.id] = null;
+        upDate();
+    });
+    
+    function upDate() {
+        io.emit('updateUserList', userList);
+        var allVotesIn = true;
 
-});
-    // Calling the update function when a clint requests it
-    // The function that updates the clients when there has been a change to the users array
-    // Moving the updated array to the users array
-    // Emitting the updated users array to the clients
-    // Checking if all votes are in if so showing votes on the clients
-io.sockets.on('update', UpDate);
-
-function UpDate(usersUpdate) {
-    users = usersUpdate;
-    io.emit('current', users);
-    !users.some((current) => current.vote === null) ? io.emit('showVotes') : null;
-};
-    //when the game master requests the votes to be cleared 
-    //removing the votes off the clients screens
-    //resetting all votes to null
-    //calling the update function to give the updated array to the clients
-io.sockets.on('clearVotes', function(){ 
-    io.emit('hideVotes');
-    users.forEach((current) => current.vote = null);
-    UpDate(users);
+        Object.keys(userList).map((key) => {
+        if(userList[key].vote === null)
+            allVotesIn = false;
+        })
+        allVotesIn ? io.emit('toggleVotes') : null;
+    };
 });
     // listening on port 8080
-const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
-});
+io.listen(PORT)
+console.log(`Listening on port ${PORT}.`);
