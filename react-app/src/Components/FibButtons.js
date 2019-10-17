0c1941184c46e4343c719974a974f8f339ef3cef
B-R0D <B-R0D@users.noreply.github.com>
2019-10-17 14:12:56 -0500

    Trying to fix

--- react-app/src/Components/FibButtons.js
+++ react-app/src/Components/FibButtons.js
@@ -1,27 +1,5 @@
 import React from 'react'
 import '../Styles/FibButtons.css'
-<<<<<<< HEAD
-
-class FibButtons extends React.Component {
-    render() {
-        return (
-            <body className='body1'>
-            <button className="myButton" id="0">0</button>
-            <button className="myButton" id="1">1</button>
-            <button className="myButton" id="2">2</button>
-            <button className="myButton" id="3">3</button>
-            <button className="myButton" id="5">5</button>
-            <button className="myButton" id="8">8</button>
-            <button className="myButton" id="13">13</button>
-            <button className="myButton" id="21">21</button>
-            <button className="myButton" id="?">?</button>
-            <br />
-        </body>
-            )
-        }
-    }
-    
-=======
 import StoryDescription from './StoryDescription';
 import ListRender from './listRender';
 import { updateVote } from '../Utils/DataService';
@@ -51,5 +29,4 @@ function showValue (e) {
     
     }
 
->>>>>>> c8b18a090828e02f9106ccb1b2aa21ee1fdb3c5a
 export default FibButtons
