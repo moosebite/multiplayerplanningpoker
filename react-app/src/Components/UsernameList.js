b8b3a057ba233d9dcce5b87dd239462df8b8734a
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-10-22 14:22:07 -0500

    .

--- react-app/src/Components/UsernameList.js
+++ react-app/src/Components/UsernameList.js
@@ -2,14 +2,39 @@ import React from 'react'
 
 import '../Styles/UsernameList.css'
 
+import { updateUserList } from '../Utils/DataService'
+
 class UsernameList extends React.Component {
+    
+state = {
+    userMap: {}
+}
+
+constructor(props) {
+    super(props);
+
+    updateUserList((updatedUserMap) => {
+        this.setState({
+            userMap: updatedUserMap
+        });
+
+        this.render();
+    });
+}
+
     render() {
         return(
             <p2>
                 (Usernames go here.)
+                <br/>
                 <button class="btn btn-primary" type="button" disabled>
                     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                    Waiting for vote
+                    Waiting For Vote
+                </button>
+                <br/>
+                <button class="btn btn-primary" type="button" disabled>
+                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    Vote Submitted
                 </button>
             </p2>
         )
