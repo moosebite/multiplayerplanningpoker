1c7dcd6fda492b82ec84c147d244a97db8815d8a
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-10-22 12:28:22 -0500

    Added .map and helper function call to FibButtons

--- react-app/src/Components/FibButtons.js
+++ react-app/src/Components/FibButtons.js
@@ -4,13 +4,15 @@ import '../Styles/FibButtons.css'
 
 import { Button, Container, Row, Col } from 'react-bootstrap'
 
+import { updateVote } from '../Utils/DataService';
+
 class FibButtons extends React.Component {
-    numbers = [0,1,2,3,5,8,13,21]
+    numbers = [0,1,2,3,5,8,13,21,'?']
     render() {
             const rowButtons = this.numbers.map((number) => {
                 return (
                     <Col xs={3}>
-                        <Button className='fibButton' variant="outline-primary">{number}
+                        <Button className='fibButton' variant="outline-primary">{number} onClick = {showValue}
                         </Button>
                     </Col>
                 )
@@ -24,4 +26,14 @@ class FibButtons extends React.Component {
             )
     }
 }
+
+//OnClick function to pass the ID of a button to update vote function
+function showValue (e) {
+    updateVote(e.target.id);
+    console.log("Value is: ", this );
+    }
+
 export default FibButtons
+
+
+
