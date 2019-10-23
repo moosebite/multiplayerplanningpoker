aa4d0dfcb2041899e7ec2dcbd1226a2f75b2d794
Steven Hearne <smh9w@mtmail.mtsu.edu>
2019-10-23 11:56:37 -0500

    Setup the login page to talk to the server.

--- react-app/src/Components/LoginPage.js
+++ react-app/src/Components/LoginPage.js
@@ -1,13 +1,27 @@
 import React from 'react'
+
 import '../Styles/LoginPage.css'
+import { addUser } from '../Utils/DataService'
 
 class LoginPage extends React.Component {
+    
+    handleSubmit = (event) => {
+        console.log('Form submitted!');
+
+        //event.preventDefault(); 
+
+        addUser(this.input.value);
+    }
+
     render() {
         return (
             <div>
                 <h1>Input your name:</h1>
-                <form action='./GameRoom'>
-                <input type='text' className='input'/>
+                <form action= './GameRoom'>
+                <input type='text' className='input1'
+                ref={(input) => this.input = input}
+                />
+                <button onClick= './GameRoom' onClick = {this.handleSubmit} >Submit</button>
                 </form>
             </div>
         )
