1e799d0d71d2030a57c4d911097bce43f41d308b
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-10-23 22:01:06 -0500

    Fixed FibButtons.js and FibButtons.css to reflect better looking css.

--- react-app/src/Components/FibButtons.js
+++ react-app/src/Components/FibButtons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import '../Styles/FibButtons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { Button, Container, Row, Col } from 'react-bootstrap'
 
@@ -17,7 +18,7 @@ class FibButtons extends React.Component {
     render() {
             const rowButtons = this.numbers.map((number) => {
                 return (
-                    <Col xs={3}>
+                    <Col xs={6} sm={3} md={2} lg={1}>
                         <Button className='fibButton' variant="outline-primary" onClick={(event) => this.handleShowValue(number)}>{number}
                         </Button>
                     </Col>
