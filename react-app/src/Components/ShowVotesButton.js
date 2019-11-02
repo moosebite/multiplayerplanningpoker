68e723ed7fe2aa927e98346daf991da0d3bb9172
Taylor Hartman <tah6k@mtmail.mtsu.edu>
2019-11-01 19:02:57 -0500

    Inserted ShowVotesButton.js/.css from the ShowVotesButton branch

---
+++ react-app/src/Components/ShowVotesButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../Styles/ShowVotesButton.css';
+
+class ShowVotesButton extends React.Component {
+    handleSubmit = (event) => {
+      this.props.dataService.showVotes();
+      console.log("Showing votes...");
+    }
+
+    render() {
+        return (
+          <div id="mybutton2">
+            <button className='ShowVotesButton' onClick = {this.handleSubmit}>{"Show votes"}</button>
+          </div>
+            )
+        }
+    }
+    
+export default ShowVotesButton;
