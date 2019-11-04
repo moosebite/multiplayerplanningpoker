acc52d5c2e1c596d0826e49cb1c386aa9db6d51c
gaw2t <gaw2t@mtmail.mtsu.edu>
2019-11-03 21:28:02 -0600

    Made and implemented underlying logic for StoryToggle

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './Styles/App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./Styles/App.css";
 
-import GameRoom from './Components/GameRoom';
-import LoginPage from './Components/LoginPage';
+import GameRoom from "./Components/GameRoom";
+import LoginPage from "./Components/LoginPage";
 
 class App extends React.Component {
-   render () {
+  render() {
     return (
       <div>
         <Switch>
-          <Route path='/GameRoom'>
-            <GameRoom/>
+          <Route path="/GameRoom">
+            <GameRoom />
           </Route>
-          <Route path='/'>
-            <LoginPage/>
+          <Route path="/">
+            <LoginPage />
           </Route>
         </Switch>
       </div>
--- react-app/src/Components/GameRoom.js
+++ react-app/src/Components/GameRoom.js
@@ -1,40 +1,42 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React from "react";
 
-import '../Styles/GameRoom.css';
-import { withRouter } from 'react-router-dom';  
-import UsernameList from './UsernameList';
-import Story from './Story';
-import FibButtons from './FibButtons';
-import DataService from '../Utils/DataService';
-import ClearVotesButton from './ClearVotesButton';
+import "../Styles/GameRoom.css";
+import { withRouter } from "react-router-dom";
+import UsernameList from "./UsernameList";
+import Story from "./Story";
+import FibButtons from "./FibButtons";
+import DataService from "../Utils/DataService";
+import ClearVotesButton from "./ClearVotesButton";
+import StoryToggle from "./StoryToggle";
 
 class GameRoom extends React.Component {
-    constructor(props) {
-        super(props);
-        if (!localStorage.getItem('username')) {
-            this.props.history.push('./LoginPage');
-        } else {
-            this.username = localStorage.getItem('username');
-            this.dataService = new DataService(this.username);
-        }
+  constructor(props) {
+    super(props);
+    if (!localStorage.getItem("username")) {
+      this.props.history.push("./LoginPage");
+    } else {
+      this.username = localStorage.getItem("username");
+      this.dataService = new DataService(this.username);
     }
+  }
 
-    render() {
-        const GameRoomElements = (
-            <div className='gameroomBackground'>
-                <Story />
-                <br />
-                <UsernameList dataService = {this.dataService} />
-                <FibButtons dataService = {this.dataService} />
-                <ClearVotesButton dataService = {this.dataService} />
-            </div>
-        );
+  render() {
+    const GameRoomElements = (
+      <div className="gameroomBackground">
+        <Story />
+        <br />
+        <UsernameList dataService={this.dataService} />
+        <FibButtons dataService={this.dataService} />
+        <ClearVotesButton dataService={this.dataService} />
+        <StoryToggle dataService={this.dataService} />
+      </div>
+    );
 
-        const redirect = <p>Redirecting to login...</p>;
+    const redirect = <p>Redirecting to login...</p>;
 
-        return this.dataService ? GameRoomElements : redirect;
-    }
+    return this.dataService ? GameRoomElements : redirect;
+  }
 }
 
 export default withRouter(GameRoom);
